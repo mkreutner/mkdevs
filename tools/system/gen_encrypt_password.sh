7dc7e4588da677d96e9a585421ada96973dc6d82
Michel Kreutner <mkreutner@gmail.com>
2023-07-02 20:41:37 +0200

    fix: typo

--- tools/system/gen_encrypt_password.sh
+++ tools/system/gen_encrypt_password.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 read -s -p "Enter Password: " pwd;
-encpwd=$(openssl passwd -1 -dtdin <<< $pwd)
+encpwd=$(openssl passwd -1 -stdin <<< $pwd)
 echo
 echo "API_PASSWD='$(pwd)'"
 echo "API_ENCRYPTED_PASSWORD='${encpwd}'"
