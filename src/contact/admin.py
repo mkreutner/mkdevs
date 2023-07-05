2f4c10a19e83e1d17c1c19369b02cb86e95b203b
Michel Kreutner <mkreutner@gmail.com>
2023-07-05 10:04:29 +0200

    dev: add contact messages admin side

--- src/contact/admin.py
+++ src/contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactMessage
+
+# Custom admin models
+class ContactMessageAdmin(admin.ModelAdmin):
+    
+    list_diplay = ['name','email','message','is_read','is_treated','created_date',]
+    search_fields = ['name','email',]
+
+    readonly_fields = ['name','email','message','created_date',]
+
+
+# Models and admin models registration
+admin.site.register(ContactMessage, ContactMessageAdmin)
